e9f8d2227a410b6f85507a5c5433231dbe8efc60
shamoun-shamoun <95720285+shamoun-shamoun@users.noreply.github.com>
2022-02-28 12:39:58 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "tf-state-bucket-shamoun"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "eu-west-1"
+    dynamodb_table = "tf-dynamodb-table-shamoun"
   }
 }
