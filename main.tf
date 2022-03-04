6d34be330f9ac6f0d5fdf11d3256eb32cbc62da8
shamoun-shamoun <95720285+shamoun-shamoun@users.noreply.github.com>
2022-03-04 15:29:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ module "aft" {
  tf_backend_secondary_region = "eu-west-2"
 
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "shamoun-shamoun/Demo-azure-devops/terraform-aft-account-request"
-  global_customizations_repo_name               = "shamoun-shamoun/Demo-azure-devops/terraform-aft-global-customizations"
-  account_customizations_repo_name              = "shamoun-shamoun/Demo-azure-devops/terraform-aft-account-customizations"
-  account_provisioning_customizations_repo_name = "shamoun-shamoun/Demo-azure-devops/terraform-aft-account-provisioning-customizations"
+  account_request_repo_name                     = "shamoun-shamoun/Demo-azure-devops/aft-account-request"
+  global_customizations_repo_name               = "shamoun-shamoun/Demo-azure-devops/aft-global-customizations"
+  account_customizations_repo_name              = "shamoun-shamoun/Demo-azure-devops/aft-account-customizations"
+  account_provisioning_customizations_repo_name = "shamoun-shamoun/Demo-azure-devops/aft-account-provisioning-customizations"
 }
   
 
