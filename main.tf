8b4b6513f64c979735f35d402c83e4d551bbc8a3
shamoun-shamoun <95720285+shamoun-shamoun@users.noreply.github.com>
2022-03-04 14:55:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,12 @@ module "aft" {
   tf_backend_secondary_region = "eu-west-2"
 
   vcs_provider                                  = "github"
+  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
+  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
+  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
+}
+ 
 }
   
 
