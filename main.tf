b589430d1c66d1e4a437efa99d16a118eec0176e
shamoun-shamoun <95720285+shamoun-shamoun@users.noreply.github.com>
2022-03-04 13:56:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,31 @@
-provider "aws" {
-  region = var.aws_region
+//  provider "aws" {
+//    region = var.region
+//    access_key = var.access_key
+//    secret_key = var.secret_key
+//    token      = var.token
+//  }
+
+
+module "aft" {
+   source                      = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+
+  ct_management_account_id    = var.ct_management_account_id
+  log_archive_account_id      = var.log_archive_account_id
+  audit_account_id            = var.audit_account_id
+  aft_management_account_id   = var.aft_management_account_id
+  ct_home_region              = var.ct_home_region
+  tf_backend_secondary_region = "us-west-2"
+
+  vcs_provider                                  = "github"
+  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
+  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
+  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
+}
+  
+  # Optional Feature Flags
+  aft_feature_delete_default_vpcs_enabled = true
+  aft_feature_cloudtrail_data_events      = false
+  aft_feature_enterprise_support          = false
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
-
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
-  tags= {
-    Name = var.tag_name
-  }
-}
-
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
